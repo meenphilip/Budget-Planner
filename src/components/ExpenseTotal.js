2c17baea47ed7ef1d543fb7642e31055303fadb6
Meen Philip <meenphilip@gmail.com>
2022-12-23 22:07:52 +0300

    summing up the expeses with reduce method

--- src/components/ExpenseTotal.js
+++ src/components/ExpenseTotal.js
@@ -1,7 +1,15 @@
+import { useContext } from 'react';
+import { AppContetxt } from '../context/AppContext';
+
 const ExpenseTotal = () => {
+  const { expenses } = useContext(AppContetxt);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return total + item.cost;
+  },0);
+
   return (
     <div className='alert alert-primary'>
-      <span>Spent so far: $960</span>
+      <span>Spent so far: ${totalExpenses}</span>
     </div>
   );
 };
--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -1,7 +1,17 @@
+import { useContext } from 'react';
+import { AppContetxt } from '../context/AppContext';
+
 const Remaining = () => {
+  const { expenses, budget } = useContext(AppContetxt);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return total + item.cost;
+  }, 0);
+
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
   return (
-    <div className='alert alert-success'>
-      <span>Remaining: $1040</span>
+    <div className={`alert ${alertType}`}>
+      <span>Remaining: ${budget - totalExpenses}</span>
     </div>
   );
 };
