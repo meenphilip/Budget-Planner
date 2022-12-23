346482741e7a7175b9282dc358621fd69f18b992
Meen Philip <meenphilip@gmail.com>
2022-12-23 22:23:10 +0300

    add delete expense handler

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,17 @@
+import { useContext } from 'react';
+import { AppContetxt } from '../context/AppContext';
 import { TiDelete } from 'react-icons/ti';
 
-const ExpenseItem=(props)=> {
+const ExpenseItem = props => {
+  const { dispatch } = useContext(AppContetxt);
+
+  const deleteExpenseHandler = e => {
+    dispatch({
+      type: 'DELETE_EXPENSE',
+      payload: props.id,
+    });
+  };
+
   return (
     <li className='list-group-item d-flex justify-content-between align-items-center'>
       {props.name}
@@ -8,10 +19,14 @@ const ExpenseItem=(props)=> {
         <span className='badge rounded-pill text-bg-primary mr-3'>
           ${props.cost}
         </span>
-        <TiDelete className='text-danger' size='1.5rem'></TiDelete>
+        <TiDelete
+          className='text-danger'
+          size='1.5rem'
+          onClick={deleteExpenseHandler}
+        ></TiDelete>
       </div>
     </li>
   );
-}
+};
 
 export default ExpenseItem;
