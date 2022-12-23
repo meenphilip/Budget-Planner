6322df253c2a5b72896bc614f6b56662fcbfd47a
Meen Philip <meenphilip@gmail.com>
2022-12-23 15:27:32 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className='container'>
+      <p className='h1 text-danger'>Hello world!</p>
+    </div>
+  );
+}
+
+export default App;
