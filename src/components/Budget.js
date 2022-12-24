a4c4529917f58a44ae232cd64bc32f5a7370cc17
Meen Philip <meenphilip@gmail.com>
2022-12-24 16:31:09 +0300

    Add edit logic

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,31 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { AppContetxt } from '../context/AppContext';
+import EditBudget from './EditBudget';
+import ViewBudget from './ViewBudget';
 
 const Budget = () => {
-  const { budget } = useContext(AppContetxt);
+  const { budget, dispatch } = useContext(AppContetxt);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const onEdithandler = () => {
+    setIsEditing(true);
+  };
+
+  const onSaveEditHandler = value => {
+    dispatch({
+      type: 'SET_BUDGET',
+      payload: value,
+    });
+    setIsEditing(false);
+  };
+
   return (
-    <div className='alert alert-secondary'>
-      <span>Budget: ${budget}</span>
+    <div className='alert alert-secondary p-3 d-flex align-items-center justify-content-between'>
+      {isEditing ? (
+        <EditBudget onSaveEditHandler={onSaveEditHandler} budget={budget} />
+      ) : (
+        <ViewBudget onEdithandler={onEdithandler} budget={budget} />
+      )}
     </div>
   );
 };
